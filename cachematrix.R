df4df7bb1c92636389dd16d5e1a5a28b491d21fd
GillesInTokyo <braxenk@yahoo.com>
2015-10-23 18:15:52 +0900

    R programming Week 2 assignment
    writing of function cacheSolve and MakeCacheMatrix for week 2 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Author Gilles Dqn - study program 2015 R Programming Language class
+#code validated on a couple of examples 2015/10/21
 
-## Write a short comment describing this function
+#--------------------------------------------------------------------------
+#Function name: makeCacheMatrix 
+#purpose      : caches inverse of matrix and the matrix itself
+#arguments    : invertible matrix (matrix assumed invertible)
+#return       : list of accesses and settings of features of "special" matrix
+#--------------------------------------------------------------------------
+makeCacheMatrix <- function (X) {
 
-makeCacheMatrix <- function(x = matrix()) {
+		#initialize data
+		inverseX <- NULL
 
+		#set matrix X in "special" matrix and initialize inverse Matrix
+		setMatrix <- function (Y) { 
+			X <<- Y 
+			inverseX <<-NULL
+		}
+
+		#get inital matrix X from "special" matrix
+		getMatrix <- function() X
+
+		#set special matrix as matrix X
+		setInverse <- function(X) {
+			inverseX <<- solve(X)
+			return (inverseX)
+						  }
+
+
+		#get inverse matrix from "special" matrix
+		getInverse <- function() inverseX   
+
+
+		#build and return "special" matrix (list)
+		list(setMatrix  = setMatrix, 
+		     setInverse = setInverse, 
+                 getMatrix  = getMatrix, 
+                 getInverse = getInverse) 	
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#--------------------------------------------------------------------------
+#Function name: cacheSolve 
+#purpose      : caches or retrieve the inverse of matrix
+#arguments    : "special" matrix
+#return       : inverse of matrix
+#--------------------------------------------------------------------------
+cacheSolve  <- function (Xspc) {
+
+		#check whether there is an inverse matrix
+		inverseX <- Xspc$getInverse()
+
+		if (!is.null(inverseX)) {
+			message("getting the inverse matrix")
+                  return(inverseX)
+                                   }
+
+		#In case did not return compute, set and return inverse matrix
+		tempX <- Xspc$getMatrix()		
+		Xspc$setInverse(tempX)
+            tempXInverse <- Xspc$getInverse()
+		
+		return(tempXInverse) 
+
 }
